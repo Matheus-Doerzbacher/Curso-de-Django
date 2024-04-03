ef1028449f2a6e445028810c6a397191c1f2c0d2
Matheus Doerzbacher <matheus.doer@gmail.com>
2024-04-03 16:37:35 -0300

    Apresentando produtos em tela

--- appBase/views.py
+++ appBase/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
 
+from .models import Produto
+
 
 def index(request):
 
-    outro = "Django é top2"
+    produtos = Produto.objects.all()
     context = {
-        "curso": "Programação Web com Django Framework",
-        "outro": outro,
+        "produtos": produtos,
     }
     return render(request, "index.html", context)
 
 
 def contato(request):
     return render(request, "contato.html")
+
+
+def produto(request, pk):
+    prod = Produto.objects.get(id=pk)
+    context = {
+        "produto": prod,
+    }
+    return render(request, "produto.html", context)
